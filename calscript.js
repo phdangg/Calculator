f0cbf626ce9eb3fce109a4c276af81d89c949d8e
unknown <dmphuc21@clc.fitus.edu.vn>
2023-11-04 11:23:29 +0700

    Add event listener to button

--- calscript.js
+++ calscript.js
@@ -13,11 +13,49 @@ function createButton(){
             const squareButton = document.createElement('div');
             squareButton.className = 'squareButton';
             squareButton.textContent = buttonList[i][j];
-            if (buttonList[i][j] == 'DEL' || buttonList[i][j] == 'AC')
+            if (buttonList[i][j] == 'DEL' || buttonList[i][j] == 'AC'){
                 squareButton.style.backgroundColor = '#3A64C1';
+                squareButton.style.color = 'white';
+            }
             buttonContainer.appendChild(squareButton);
         }
     }
 }
-
 createButton();
+
+
+const allSquareButton = document.querySelectorAll('.squareButton');
+const topScreen = document.querySelector('.top-screen');
+const bottomScreen = document.querySelector('.bottom-screen');
+let topScreenString = "";
+let bottomScreenString = "";
+
+
+function printResult(topScreenString){
+
+}
+
+allSquareButton.forEach(squareButton => {
+    squareButton.addEventListener('click',()=>{
+        switch(squareButton.textContent){
+            case 'AC':
+                topScreen.textContent = "";
+                topScreenString = "";
+                return;
+            case 'DEL':
+                topScreenString = topScreen.textContent;
+                topScreenString = topScreenString.substring(0,topScreenString.length-1)
+                topScreen.textContent = topScreenString;
+                return;
+            case '=':
+                printResult(topScreenString);
+                return;
+            case 'x10':
+            case 'Ans':
+            default:
+                break;
+        };
+        topScreenString += squareButton.textContent;
+        topScreen.textContent = topScreenString;
+    });
+});
